55d3e0552edfd5efa7187e5ecbde1e696e804b24
ShivaEslami97 <shivaslami@gmail.com>
2022-05-27 23:59:06 +0430

    Add subscription part

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+////// Date picker
 const today = new Date().setHours(0, 0, 0, 0);
 const defaultOptions = {
     defaultViewDate: today,
@@ -7,3 +8,18 @@ const defaultOptions = {
 console.log(today);
 const elem = document.getElementById('inline');
 const datepicker = new Datepicker(elem, defaultOptions);
+
+////// sticky menu
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+function myFunction() {
+    if (window.scrollY >= 50) {
+        header.classList.add("is-sticky");
+    } else {
+        header.classList.remove("is-sticky");
+    }
+}
+document.addEventListener("scroll", () => {
+    /// sticky menu
+    myFunction();
+});
