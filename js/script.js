bfde9806f233c0108cd49a6752613b446f4a2a78
Shiva Eslami <shivaslami@gmail.com>
2022-12-05 21:33:00 +0330

    Update responsive menu

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 ////// Date picker
 const today = new Date().setHours(0, 0, 0, 0);
 const defaultOptions = {
-    defaultViewDate: today,
-    nextArrow: '<i class="fa-solid fa-angle-right"></i>',
-    prevArrow: '<i class="fa-solid fa-angle-left"></i>',
+  defaultViewDate: today,
+  nextArrow: '<i class="fa-solid fa-angle-right"></i>',
+  prevArrow: '<i class="fa-solid fa-angle-left"></i>',
 };
 console.log(today);
-const elem = document.getElementById('inline');
+const elem = document.getElementById("inline");
 const datepicker = new Datepicker(elem, defaultOptions);
 
 ////// sticky menu
 var header = document.getElementById("header");
 var sticky = header.offsetTop;
 function myFunction() {
-    if (window.scrollY >= 50) {
-        header.classList.add("is-sticky");
-    } else {
-        header.classList.remove("is-sticky");
-    }
+  if (window.scrollY >= 50) {
+    header.classList.add("is-sticky");
+  } else {
+    header.classList.remove("is-sticky");
+  }
 }
 // document.addEventListener("scroll", () => {
 //     /// sticky menu
@@ -25,25 +25,42 @@ function myFunction() {
 // });
 
 // menu tabs
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll(".nav__link");
 navLink.forEach((item, index) => {
-    item.addEventListener('click', function (e) {
-        console.log(item);
-        navLink.forEach(navItem => {
-            navItem.classList.remove('active');
-        });
-        item.classList.add('active');
-    })
+  item.addEventListener("click", function (e) {
+    console.log(item);
+    navLink.forEach((navItem) => {
+      navItem.classList.remove("active");
+    });
+    item.classList.add("active");
+  });
 });
 
 // Homework Progress
-const progressItem = document.querySelectorAll('.progress__item');
+const progressItem = document.querySelectorAll(".progress__item");
 
 progressItem.forEach((item, index) => {
-    item.addEventListener('click', function (e) {
-        progressItem.forEach(progress => {
-            progress.classList.remove('progress--active');
-        });
-        item.classList.add('progress--active');
-    })
+  item.addEventListener("click", function (e) {
+    progressItem.forEach((progress) => {
+      progress.classList.remove("progress--active");
+    });
+    item.classList.add("progress--active");
+  });
+});
+
+///// mobile menu
+const menu = document.querySelector(".nav");
+const menuTrigger = document.querySelector(".header__mobile-btn");
+const menuClosed = document.querySelector(".menu-mobile-close");
+
+function toggleMenu() {
+  menu.classList.toggle("active");
+  menuOverlay.classList.toggle("active");
+}
+
+menuTrigger.addEventListener("click", () => {
+  toggleMenu();
+});
+menuClosed.addEventListener("click", () => {
+  toggleMenu();
 });
